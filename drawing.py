7e7550183f0f3e7d87f6927115e16290080db656
RafSardarian <raf.sardarian@gmail.com>
2022-09-29 00:15:29 +0400

    'update'

--- drawing.py
+++
@@ -1,6 +0,0 @@
-lst = [4,2,2,2,4]
-for i in range(len(lst)):
-    if i == 0 or i == len(lst) - 1:
-        print('*' * lst[i])
-        continue
-    print('*  ' * lst[i])
--- random_nums.py
+++
@@ -1,8 +0,0 @@
-import random
-array = []
-while len(array) < 50:
-    array.append(random.randint(3,6))
-
-print(array)
-
-
